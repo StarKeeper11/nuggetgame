3533b1c238e9f0a9bf8e0ee3f8e37d33cf09a16f
StarKeeper11 <iakuninvova11@gmail.com>
2025-12-25 14:00:23 -0800

    cooking

---
+++ player.gd
@@ -0,0 +1,15 @@
+extends RigidBody3D
+
+var plr_speed = 1200.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var input := Vector3.ZERO
+	input.x = Input.get_axis("move_left", "move_right")
+	input.z = Input.get_axis("move_forward", "move_back")
+	apply_central_force(input * plr_speed * delta)
