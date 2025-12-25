21e7208e544a076e5bc7d0452e893254c959dccc
StarKeeper11 <iakuninvova11@gmail.com>
2025-12-25 14:19:41 -0800

    More cooking

--- player.gd
+++ player.gd
@@ -4,7 +4,7 @@ var plr_speed = 1200.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -12,4 +12,12 @@ func _process(delta: float) -> void:
 	var input := Vector3.ZERO
 	input.x = Input.get_axis("move_left", "move_right")
 	input.z = Input.get_axis("move_forward", "move_back")
+	
+	if input.x != 0.0 and input.z != 0.0:
+		input.x = (input.x)/1.4
+		input.z = (input.z)/1.4
+		
 	apply_central_force(input * plr_speed * delta)
+	
+	if Input.is_action_just_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
